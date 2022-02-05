ab1e37070f89af4b6c5a2d064f6c5adeea906f92
winkxx <68945988+winkxx@users.noreply.github.com>
2022-02-05 20:54:45 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ touch /root/.config/rclone/rclone.conf
 echo "$conf" >>/root/.config/rclone/rclone.conf
 nginx -c /etc/nginx/nginx.conf
 nginx -s reload
+rclone rcd --rc-user=xxr --rc-pass=991211 --rc-allow-origin="http://localhost:4200"
 git clone https://github.com/ElonH/RcloneNg.git
 cd RcloneNg
 npm install # NodeJs version >= 10
