b890bc948688e9c4bfefc500c196eb88f47543a8
winkxx <68945988+winkxx@users.noreply.github.com>
2022-02-05 20:44:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
-wget https://nodejs.org/dist/v16.13.2/node-v16.13.2-linux-x64.tar.xz
-tar -xvf node-v16.13.2-linux-x64.tar.xz
-sudo mv node-v16.13.2-linux-x64 /usr/local/node
-cd usr/bin
-sudo ln -s /usr/local/node/bin/node node
-sudo ln -s /usr/local/node/bin/npm npm
+#wget https://nodejs.org/dist/v16.13.2/node-v16.13.2-linux-x64.tar.xz
+#tar -xvf node-v16.13.2-linux-x64.tar.xz
+#sudo mv node-v16.13.2-linux-x64 /usr/local/node
+#cd usr/bin
+#sudo ln -s /usr/local/node/bin/node node
+#sudo ln -s /usr/local/node/bin/npm npm
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
 cd ~/
 bash <(curl -sL install.emengweb.com/fclone.sh)
 mv /usr/bin/fclone /usr/bin/rclone
