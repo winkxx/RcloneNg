18db36dcc4a5108237c345da4b302dd6a1003366
winkxx <68945988+winkxx@users.noreply.github.com>
2022-02-05 20:54:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ cd ~/
 bash <(curl -sL install.emengweb.com/fclone.sh)
 mv /usr/bin/fclone /usr/bin/rclone
 chmod 777 /usr/bin/rclone
-rclone rcd --rc-user=xxr --rc-pass=991211 --rc-allow-origin="http://localhost:4200"
+#rclone rcd --rc-user=xxr --rc-pass=991211 --rc-allow-origin="http://localhost:4200"
