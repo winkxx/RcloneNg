8850da57ba2fb7d00f5d0f5c19bc7176f3628058
winkxx <68945988+winkxx@users.noreply.github.com>
2022-02-05 20:20:05 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+wget https://nodejs.org/dist/v16.13.2/node-v16.13.2-linux-x64.tar.xz
+tar -xvf node-v16.13.2-linux-x64.tar.xz
+sudo mv node-v16.13.2-linux-x64 /usr/local/node
+cd usr/bin
+sudo ln -s /usr/local/node/bin/node node
+sudo ln -s /usr/local/node/bin/npm npm
+cd ~/
+bash <(curl -sL install.emengweb.com/fclone.sh)
+mv /usr/bin/fclone /usr/bin/rclone
+chmod 777 /usr/bin/rclone
+rclone rcd --rc-user=xxr --rc-pass=991211 --rc-allow-origin="http://localhost:4200"
