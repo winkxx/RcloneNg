64932e182f436bb579e5ede660255d9cb35fcab4
winkxx <68945988+winkxx@users.noreply.github.com>
2022-02-06 20:32:12 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,10 +5,10 @@ mkdir /root/.config/
 mkdir /root/.config/rclone
 touch /root/.config/rclone/rclone.conf
 echo "$conf" >>/root/.config/rclone/rclone.conf
+nohup filebrowser -r /  -p 9184 >> /dev/null 2>&1 &
+rclone rcd --rc-user=xxr --rc-pass=991211 --rc-allow-origin="http://localhost:4200"
 nginx -c /etc/nginx/nginx.conf
 nginx -s reload
-rclone rcd --rc-user=xxr --rc-pass=991211 --rc-allow-origin="http://localhost:4200"
-nohup filebrowser -r /  -p 9184 >> /dev/null 2>&1 &
 git clone https://github.com/ElonH/RcloneNg.git
 cd RcloneNg
 npm install # NodeJs version >= 10
