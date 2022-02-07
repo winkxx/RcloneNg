51a225f5575fde4e550c71a7f976f0dfcba5e570
winkxx <68945988+winkxx@users.noreply.github.com>
2022-02-07 20:43:47 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ mkdir /root/.config/rclone
 touch /root/.config/rclone/rclone.conf
 echo "$conf" >>/root/.config/rclone/rclone.conf
 nohup filebrowser -r /  -p 9184 >> /dev/null 2>&1 &
-nohup rclone rcd --rc-web-gui --rc-user=xxr --rc-pass=991211 &
+nohup rclone rcd --rc-addr=127.0.0.1:5572 --rc-user=xxr --rc-pass=991211 --rc-allow-origin="https://elonh.github.io" &
 nginx -c /etc/nginx/nginx.conf
 nginx -s reload
 #git clone https://github.com/ElonH/RcloneNg.git
